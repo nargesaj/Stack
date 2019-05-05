b61ef4fa3a1f9e0e19031d23febdcbdc1e0fcc56
Narges Aghakazem <naghakazem@gmail.com>
2019-05-05 17:43:01 -0400

    Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            for (int i = 1; i < 5; i++)
+                stack.Push(i);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+
+            stack.Pop();
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+    public class Stack
+    {
+        private readonly ArrayList _stack;
+
+        public Stack()
+        {
+            _stack = new ArrayList();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Cannot push a null object to the stack!");
+
+            _stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+                throw new InvalidOperationException("Stack is empty. Cannot pop from an empty stack!");
+
+            var index = _stack.Count - 1;
+
+            var obj = _stack[index];
+
+            _stack.RemoveAt(index);
+
+            return obj;
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
